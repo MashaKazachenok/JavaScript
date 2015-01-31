1e4eab2c4c465ed638f1d10f56bb84da1c08e0a1
m.kazachenok <kazachenokm@mail.ru>
2015-01-31 22:49:58 +0300

    УРААА!!! работает

--- Список дел/script.js
+++ Список дел/script.js
@@ -1,5 +1,12 @@
 function addTask (argument) {
+	// получаем описание дела
 	var task = document.getElementById('task').value;
+	// создаем пустой параграф
+	var taskNode = document.createElement('p');
+	// добавим описание дела в парграф
+	taskNode.innerText = task;
+	// получаем контейнер списка дел
 	var container = document.getElementById('list');
-	container.innerText = task;
+	// вставить параграф с описанием дела в контейнет списка дел
+	container.appendChild(taskNode);
 }
