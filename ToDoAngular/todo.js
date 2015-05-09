41cb95b4d5e3feeb8e39c7ae8e48e0578bb09101
m.kazachenok <kazachenokm@mail.ru>
2015-05-09 18:12:10 +0300

    fix save

--- ToDoAngular/todo.js
+++ ToDoAngular/todo.js
@@ -1,21 +1,31 @@
  var example = angular.module("example", ["ngStorage"]);
  example.controller("ExampleController", function($scope, $localStorage) {
 
- 	$scope.todoList =  JSON.parse($localStorage.list)
-	
-	$scope.addTask = function()
-	{
-		if(!$scope.newTask)return;
-		$scope.todoList.push({text:$scope.newTask, done:false});
-		$localStorage.list = JSON.stringify($scope.todoList);
-		$scope.newTask= "";
-	};
+
+ 	if (!$localStorage.list){
+ 		$scope.todoList=[]
+ 	} else {
+ 		$scope.todoList =  JSON.parse($localStorage.list)
+ 	};
+
+ 	$scope.addTask = function()
+ 	{
+ 		if(!$scope.newTask)return;
+ 		$scope.todoList.push({text:$scope.newTask, done:false});
+ 		$localStorage.list = JSON.stringify($scope.todoList);
+ 		$scope.newTask= "";
+ 	};
 
  	$scope.clearCompleted = function()
  	{
  		$scope.todoList = _.filter($scope.todoList, function(todo){
  			return !todo.done;
  		});
+ 		$localStorage.list = JSON.stringify($scope.todoList);
+ 	};
+
+ 	$scope.changeCheckbox = function()
+ 	{
  		$localStorage.list = JSON.stringify($scope.todoList);
  	};
  });
