5c81f38c47f6d65e36ad153e3513a84f6c80a112
m.kazachenok <kazachenokm@mail.ru>
2015-05-09 13:19:29 +0300

    fix save and display tasks

--- ToDoAngular/todo.js
+++ ToDoAngular/todo.js
@@ -1,50 +1,21 @@
  var example = angular.module("example", ["ngStorage"]);
  example.controller("ExampleController", function($scope, $localStorage) {
 
- 	$scope.todoList =  
- 	[
- 	{  text:"goooo", done:true },
- 	{  text:JSON.parse(localStorage.getItem("ngStorage-text")), done:$localStorage.done}
- 	];
-
- 	//$scope.todoList.text =  $localStorage.text;
- 	//$scope.todoList.done = $localStorage.done;
-
- 	$scope.addTask = function(){
-
- 		$scope.todoList.push({text:$scope.newTask, done:false});
-
- 		if($localStorage.text !=null){
-
- 			$localStorage.text = $localStorage.text + JSON.stringify($scope.newTask);
- 		} else {
- 			$localStorage.text = JSON.stringify($scope.newTask);
- 		}
-
- 		if($localStorage.done!= null) {
- 			$localStorage.done = $localStorage.done + JSON.stringify(false);
- 		} else { 
- 			$localStorage.done = false
- 		}
- 	};
-
- 	
+ 	$scope.todoList =  JSON.parse($localStorage.list)
+	
+	$scope.addTask = function()
+	{
+		if(!$scope.newTask)return;
+		$scope.todoList.push({text:$scope.newTask, done:false});
+		$localStorage.list = JSON.stringify($scope.todoList);
+		$scope.newTask= "";
+	};
 
  	$scope.clearCompleted = function()
  	{
  		$scope.todoList = _.filter($scope.todoList, function(todo){
  			return !todo.done;
  		});
- 	};
-
- 	$scope.getActive = function(){
-
-
- 	};
-
- 	$scope.getCompleted = function()
- 	{
-
-
+ 		$localStorage.list = JSON.stringify($scope.todoList);
  	};
  });
